b75f492c0448846a01f72ea54a44dec0fb865930
NickvanderRee <112868944+NickvanderRee@users.noreply.github.com>
2022-11-16 13:42:34 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+
+    </head>
+
+    <body>
+    <?php
+
+    //  Een voorbeeld van een echo en een print
+    echo "<h1>Hallo wereld,</h1>";
+    echo "Ik zit op het ";
+
+    // Voorbeeld van een variabele in PHP.
+    $schoolnaam = "Techniek College Rotterdam !";
+
+    // Met echo kun je iets op het scherm tonen.
+    echo "$schoolnaam";
+
+    echo "<h1>Hallo daar</h1>";
+    echo "ik ben ";
+
+    $naam = "Nick";
+
+    echo $naam;
+
+    ?>
+    </body>
+</html>
