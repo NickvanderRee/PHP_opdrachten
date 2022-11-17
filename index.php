ae9950ab6b9494b680ba73d68bde2e3692c39c48
NickvanderRee <112868944+NickvanderRee@users.noreply.github.com>
2022-11-17 13:14:33 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -16,13 +16,39 @@
     // Met echo kun je iets op het scherm tonen.
     echo "$schoolnaam";
 
-    echo "<h1>Hallo daar</h1>";
-    echo "ik ben ";
-
-    $naam = "Nick";
+    echo "<h1>Hallo</h1>";
+    echo "Het is ";
+    //dit geeft de variabele een waarde.
+    $naam = "vandaag ";
 
     echo $naam;
 
+    //dit bepaald de datum
+    echo "Het is vandaag " . date("l") ." ". date("Y/m/d") . "<br>";
+ 
+echo "<br>";
+ 
+echo "Vandaag is het de " . date("z"). "de dag van het jaar<br>";
+ 
+echo "<br>";
+ 
+echo  date("l") . " is de " . date("w") ."de dag van de week<br>";
+ 
+echo "<br>";
+ 
+echo "De maand " . date("F"). " heeft in totaal " . date("t"). " dagen";
+ 
+echo "<br>";
+echo "<br>";
+ 
+$DATE =  date("L");
+echo "Het jaar " . date("Y"). " en dat jaar is ";
+if($DATE == 1) {
+    echo "een schrikkeljaar";
+}
+else {
+    echo " geen schrikkeljaar";
+}
     ?>
     </body>
 </html>
