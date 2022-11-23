4ecaf3dd0aac2ae9be8a89692a9f5fee0dbbef55
NickvanderRee <112868944+NickvanderRee@users.noreply.github.com>
2022-11-23 12:17:03 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,54 +1,29 @@
+<!DOCTYPE html>
 <html>
-    <head>
+<head>
 
-    </head>
-
+</head>
     <body>
-    <?php
+        <?php
+        //Opdracht 4.4: maak een script met producten en prijzen.
+        // Verhoog producten met 19% als ze hoger zijn dan 150 euro .Producten die goedkoper zijn dan 55 euro worden 11% duurder.
 
-    //  Een voorbeeld van een echo en een print
-    echo "<h1>Hallo wereld,</h1>";
-    echo "Ik zit op het ";
+        $product = 60;
 
-    // Voorbeeld van een variabele in PHP.
-    $schoolnaam = "Techniek College Rotterdam !";
+        if($product > 150)
+        {
+           echo $product * 1.19;
+        }
 
-    // Met echo kun je iets op het scherm tonen.
-    echo "$schoolnaam";
+        else if($product < 55)
+        {
+          echo  $product * 1.11;
+        }
+        else if($product > 55)
+        {
+          echo  $product;
+        }
 
-    echo "<h1>Hallo</h1>";
-    echo "Het is ";
-    //dit geeft de variabele een waarde.
-    $naam = "vandaag ";
-
-    echo $naam;
-
-    //dit bepaald de datum
-    echo "Het is vandaag " . date("l") ." ". date("Y/m/d") . "<br>";
- 
-echo "<br>";
- 
-echo "Vandaag is het de " . date("z"). "de dag van het jaar<br>";
- 
-echo "<br>";
- 
-echo  date("l") . " is de " . date("w") ."de dag van de week<br>";
- 
-echo "<br>";
- 
-echo "De maand " . date("F"). " heeft in totaal " . date("t"). " dagen";
- 
-echo "<br>";
-echo "<br>";
- 
-$DATE =  date("L");
-echo "Het jaar " . date("Y"). " en dat jaar is ";
-if($DATE == 1) {
-    echo "een schrikkeljaar";
-}
-else {
-    echo " geen schrikkeljaar";
-}
-    ?>
+        ?>
     </body>
 </html>
